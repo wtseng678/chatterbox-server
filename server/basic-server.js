2c54e2eb7f8c6bebfa08490c53dea275751e70a1
Warren Tseng <wtseng678@gmail.com>
2018-07-03 18:50:34 -0700

    Partially implemented file system

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,42 +1,77 @@
 /* Import node's http module: */
 var http = require('http');
 var requests = require('./request-handler.js');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Every server needs to listen on a port with a unique number. The
+  // standard port for HTTP servers is port 80, but that port is
+  // normally already claimed by another server and/or not accessible
+  // so we'll use a standard testing port like 3000, other common development
+  // ports are 8080 and 1337.
+  var port = 3000;
+
+  // For now, since you're running this server on your local machine,
+  // we'll have it listen on the IP address 127.0.0.1, which is a
+  // special address that always refers to localhost.
+  var ip = '127.0.0.1';
+
+  var router = {
+    '/classes/messages': requests.requestHandler
+  };
+
+  var headers = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10, // Seconds.
+    'Content-Type': 'application/json'
+  };
 
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
-var port = 3000;
+  // We use node's http module to create a server.
+  //
+  // The function we pass to http.createServer will be used to handle all
+  // incoming requests.
+  //
+  // After creating the server, we will tell it to listen on the given port and IP. */
+  var server = http.createServer(function(req, res) {
+    console.log('Serving request type ' + req.method + ' for url ' + req.url);
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
-var ip = '127.0.0.1';
+    var route = router[url.parse(req.url).pathname];
+    if (route) {
+      route(req, res);
+    } else {
+      res.writeHead(404, headers);
+      res.end('');
+    }
+  });
+  console.log('Listening on http://' + ip + ':' + port);
+  server.listen(port, ip);
 
+  // To start this server, run:
+  //
+  //   node basic-server.js
+  //
+  // on the command line.
+  //
+  // To connect to the server, load http://127.0.0.1:3000 in your web
+  // browser.
+  //
+  // server.listen() will continue running as long as there is the
+  // possibility of serving more requests. To stop your server, hit
+  // Ctrl-C on the command line.
 
-
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(requests.requestHandler);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
-
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
-
+}
--- server/request-handler.js
+++ server/request-handler.js
@@ -13,6 +13,8 @@ this file and include it in basic-server.js so that it actually works.
 **************************************************************/
 
 var url = require('url');
+var fs = require('fs');
+
 var data = { results: [] };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -24,11 +26,12 @@ var data = { results: [] };
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
+defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
 };
 
 var currentId = 1;
@@ -66,11 +69,23 @@ exports.requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  //response.writeHead(statusCode, headers);
+  
+  //fs.writeFile('classes/messages/messages.txt', 'hello world', (err) => { if (err) { throw err; } console.log('file saved'); });
+  
+  //fs.readFileSync(new URL('file://Users/warrentseng/Documents/GitHub/hrsf98-chatterbox-server/client/index.html'));
+  /*fs.readFile('../client/index.html', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    res.end();
+  });*/
+  // ../client/index.html
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -79,27 +94,54 @@ exports.requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  if (url.parse(request.url).pathname === '/classes/messages') {
-    if (request.method === 'GET') {
-      var q = url.parse(request.url, true).query;
-      if (q.order && q.order.charAt(0) === '-') {
-        data.results.sort(function(a, b) { return b.createdAt - a.createdAt; });
+  
+  if (url.parse(request.url).pathname.slice(0, 8) === '/classes') {
+    if (url.parse(request.url).pathname === '/classes/messages') {
+      if (request.method === 'GET') {
+        statusCode = statusCode || 200;
+        response.writeHead(statusCode, headers);
+        response.end(JSON.stringify(data));
+      } else if (request.method === 'POST' || request.method === 'OPTIONS') {
+        request.on('data', function(input) {
+          var d = JSON.parse(input);
+          d.objectId = getObjectId();
+          d.createdAt = new Date();
+          data.results.push(d);
+        });
+        response.writeHead(201, headers);
+        response.end(JSON.stringify(data));
       }
-      response.end(JSON.stringify(data));
+    } else {
+      response.writeHead(404, headers);
+      response.end();
     }
-    
-    if (request.method === 'POST' || request.method === 'OPTIONS') {
-      request.on('data', function(input) {
-        var d = JSON.parse(input);
-        d.objectId = getObjectId();
-        d.createdAt = new Date();
-        data.results.push(d);
-      });
-      response.writeHead(201, headers);
-      response.end(JSON.stringify(data));
-    }
+  } else if (url.parse(request.url).pathname === '/') {
+    headers['Content-Type'] = "text/html";
+
+    //read in the file and send (async)
+    fs.readFile("./client/index.html", {encoding: 'utf8'}, function (err,data) {
+      if (err) {
+        statusCode = 404;
+      } 
+      response.writeHead(statusCode, {'Content-Type': 'text/html'});
+      response.end(data);
+    });    
   } else {
-    response.writeHead(404, headers);
-    response.end();
+    if (url.parse(request.url).pathname.slice(-3) === 'css') {
+      var contentType = "text/css";
+    } else {
+      var contentType = "text/javscript";
+    }
+
+    //read file and send
+    fs.readFile("./client"+url.parse(request.url).pathname, {encoding: 'utf8'}, function (err,data) {
+      if (err) {
+        //console.log('there is an error')
+        statusCode = 404;
+      }
+      response.writeHead(statusCode, {'Content-Type': contentType});
+      response.end(data);
+    });    
   }
+
 };
